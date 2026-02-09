6af8adafa6a46baefb879f1461aca0248c7f350b
Khaled HLEIS <khaled.hleis@ensta.fr>
2026-02-09 12:15:54 +0100

    singleton metaclass and clock class where added

---
+++ backend/simulation/clock.py
@@ -0,0 +1,24 @@
+from metaclasses.singleton import Singleton
+
+class CLock(metaclass=Singleton):
+    """
+    Singleton clock class.
+
+    Keeps a global timestamp and applies a conversion factor
+    when incrementing time.
+    
+    timestamp is an integer in [ms] milliseconds, 
+    and the conversion_factor that multiplies the increment value for example it could be the sampling rate x1000.
+    """
+    def __init__(self):
+        self.__time_stamp: int = 0
+        self.conversion_factor: int = 1
+
+    def get_time_stamp(self) -> int:
+        return self.__time_stamp
+
+    def increment_time(self, delta: int = 1):
+        self.__time_stamp += delta * self.conversion_factor
+
+    def set_conversion_factor(self, factor: int):
+        self.conversion_factor = factor
